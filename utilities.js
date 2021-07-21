0c4be94fe499f270f503a327057f8f64b7b6cb89
Leandro Murgo <leandrouni01@gmail.com>
2021-07-21 15:08:08 -0300

    Cambiado utilities

--- utilities.js
+++ utilities.js
@@ -1,3 +1,7 @@
 function firstToUpperCase(string) {
   return typeof string == "string" ? string.charAt(0).toUpperCase() + string.slice(0) : "";
 }
+
+function otraFuncion() {
+  return "otra Funcion";
+}
