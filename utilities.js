bd13812000c08609d953345883445bc868acafb6
Jorge <jorgediazok@gmail.com>
2021-07-21 15:07:30 -0300

    added alert function in utilities.js

--- utilities.js
+++ utilities.js
@@ -1,3 +1,9 @@
 function firstToUpperCase(string) {
-  return typeof string == "string" ? string.charAt(0).toUpperCase() + string.slice(0) : "";
+  return typeof string == 'string'
+    ? string.charAt(0).toUpperCase() + string.slice(0)
+    : '';
+}
+
+function showAlert() {
+  alert('Hello world!');
 }
